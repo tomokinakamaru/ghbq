22324657db3da2f48c686f639c5e46ec394f5f9f
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2021-09-07 23:43:53 +0900

    Create split.py

---
+++ src/ghbq/split.py
@@ -0,0 +1,11 @@
+from os.path import splitext
+from .util import arg
+from .util import FileList
+
+
+@arg('path', help='path to file list', metavar='<path>')
+def main(args):
+    name, ext = splitext(args.path)
+    f1, f2 = FileList.load(args.path).split()
+    f1.save(f'{name}.a{ext}')
+    f2.save(f'{name}.b{ext}')
