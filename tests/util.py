10479e13a2f3928abaf3c40da103d081f77b48fc
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2021-09-08 09:39:41 +0900

    Create util.py

---
+++ tests/util.py
@@ -0,0 +1,20 @@
+import sys
+from textwrap import dedent
+from unittest.mock import patch
+
+
+def fileread(path):
+    with open(path) as f:
+        return f.read()
+
+
+def filewrite(path, text):
+    text = text.lstrip('\n')
+    text = dedent(text).strip()
+    with open(path, 'w') as f:
+        print(text, file=f)
+
+
+def patch_argv(*args):
+    argv = sys.argv[:1] + list(map(str, args))
+    return patch('argparse._sys.argv', argv)
